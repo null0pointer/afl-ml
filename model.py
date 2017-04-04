92e608479621ed14a1a968120c22bf51bc4c4df3
Sam Watson <sam.watson@MEL01-UMAC018.local>
2017-04-04 15:27:45 +1000

    Added batching and some other code tweaks

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import math
 from random import randrange
 import file_utils as fu
+import matplotlib.pyplot as plt
 
 def random_split(input, rate):
     first = []
@@ -40,6 +41,13 @@ def link_weights_and_biases(inputs, weights, biases, activation=None, pkeep=None
 def create_weight(in_size, out_size):
     return tf.Variable(tf.truncated_normal([in_size, out_size], stddev=0.1))
 
+def minibatch(data, start, length):
+    start_index = start % len(data)
+    end_index = start_index + length
+    nloops = math.ceil(end_index / len(data))
+    looped_data = nloops * data
+    return looped_data[start_index:end_index]
+
 raw_inputs = fu.read_csv(fu.csv_inputs_path())
 raw_train_ins, raw_test_ins = random_split(raw_inputs, 0.8)
 
@@ -71,7 +79,7 @@ Y_ = tf.placeholder(tf.float32, [None, 2])
 lr = tf.placeholder(tf.float32)
 pkeep = tf.placeholder(tf.float32)
 
-layer_sizes = [input_size, input_size, input_size, input_size, output_size]
+layer_sizes = [input_size, 30, 20, 10, output_size]
 weights, biases = create_weights_and_biases(layer_sizes)
 
 Ylogits = link_weights_and_biases(X, weights, biases, activation=tf.nn.relu, pkeep=pkeep)
@@ -89,20 +97,42 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(10000):
+xaxis = []
+train_accuracies = []
+test_accuracies = []
+train_loss = []
+test_loss = []
+
+BATCH_SIZE = 1000
+ITERATIONS = 4000
+for i in range(ITERATIONS):
+    train_batch_inputs = minibatch(train_inputs, i * BATCH_SIZE, BATCH_SIZE)
+    train_batch_labels = minibatch(train_labels, i * BATCH_SIZE, BATCH_SIZE)
+
     max_lr = 0.003
     min_lr = 0.0001
-    decay_period = 4000
+    decay_period = 500
     learning_rate = min_lr + (max_lr - min_lr) * math.exp(-i/decay_period)
 
-    if i % 100 == 0 or i == 9999:
+    if i % 10 == 0 or i == ITERATIONS - 1:
+        xaxis.append(i)
         a, c = sess.run([accuracy, cross_entropy], {X: train_inputs, Y_: train_labels, pkeep: 1.0})
-        print("Epoch: " + str(i) + "\n\tTrain Accuracy: " + str(a) + " Loss: " + str(c))
+        train_accuracies.append(a)
+        train_loss.append(c)
+        print("Iteration: " + str(i) + "\n\tTrain Accuracy: " + str(a) + " Loss: " + str(c))
 
         a, c, y = sess.run([accuracy, cross_entropy, Y], {X: test_inputs, Y_: test_labels, pkeep: 1.0})
+        test_accuracies.append(a)
+        test_loss.append(c)
         print("\t Test Accuracy: " + str(a) + " Loss: " + str(c))
-        print(test_inputs[0])
-        print(test_labels[0])
-        print(y[0])
 
-    sess.run(train_step, {X:train_inputs, Y_:train_labels, pkeep:0.65, lr:learning_rate})
+    sess.run(train_step, {X:train_batch_inputs, Y_:train_batch_labels, pkeep:0.65, lr:learning_rate})
+
+plt.plot(xaxis, train_accuracies, 'blue')
+plt.plot(xaxis, test_accuracies, 'red')
+plt.title('Accuracy')
+plt.show()
+plt.plot(xaxis, train_loss, 'blue')
+plt.plot(xaxis, test_loss, 'red')
+plt.title('Loss')
+plt.show()
